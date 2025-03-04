83a82998c18a8c5e8c120263bd2e3ca7a3815091
sohelrana7 <sohel.rana.web1@gmail.com>
2025-03-04 16:22:34 +0600

    watchList api create stored data in mongodg

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -28,11 +28,32 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const reviewCollection = client.db("gameDB").collection("reviews");
+    const watchListCollection = client.db("gameDB").collection("watchList");
 
+    // watch List related api
+    app.get("/watchLists", async (req, res) => {
+      const cursor = watchListCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/watchLists", async (req, res) => {
+      const watchList = req.body;
+      // console.log(watchList);
+      const result = await watchListCollection.insertOne(watchList);
+      res.send(result);
+    });
     // review related api
     app.get("/reviews", async (req, res) => {
       const cursor = reviewCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/reviews/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await reviewCollection.findOne(query);
       res.send(result);
     });
 
